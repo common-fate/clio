b0c8ce844091b9584cd8f61a7f842a1cb37dda39
chrnorm <17420369+chrnorm@users.noreply.github.com>
2022-10-25 13:57:22 +0100

    remove noprefix logging name

--- cliolog/success.go
+++ cliolog/success.go
@@ -5,8 +5,4 @@ const (
 	// messages with a [✔] symbol rather than their regular
 	// log level.
 	SuccessName = "clio.success"
-
-	// NoPrefix is a designated logging name which prints
-	// messages without any [] prefix.
-	NoPrefixName = "clio.noprefix"
 )
