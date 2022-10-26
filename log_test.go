6d45065e5da34499c67cf4897cc44774b6fc19c4
chrnorm <17420369+chrnorm@users.noreply.github.com>
2022-10-26 09:16:05 +0100

    add test for Log

--- log_test.go
+++ log_test.go
@@ -31,6 +31,22 @@ func TestError(t *testing.T) {
 
 	want := "[✘] my message\n"
 	if got != want {
-		t.Errorf("Info() = %q, want %q", got, want)
+		t.Errorf("Error() = %q, want %q", got, want)
+	}
+}
+
+func TestLog(t *testing.T) {
+	var b bytes.Buffer
+	SetWriter(&b)
+	NoColor = true
+
+	Log("my message")
+
+	got := b.String()
+
+	// should print without a prefix.
+	want := "my message\n"
+	if got != want {
+		t.Errorf("Log() = %q, want %q", got, want)
 	}
 }
