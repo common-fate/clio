539d17a2c7c7e54c658d9797d3558bb0ee2c775b
Manish Chaulagain <manish23chaulagain@gmail.com>
2023-08-01 15:36:24 +1000

    Add ability to write logs to file using lumberjack pkg

--- cliolog/new.go
+++ cliolog/new.go
@@ -7,11 +7,13 @@ import (
 	"github.com/mattn/go-colorable"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 type Options struct {
-	Writer  io.Writer
-	NoColor *bool
+	Writer          io.Writer
+	NoColor         *bool
+	FileWriteSyncer *zapcore.WriteSyncer
 }
 
 // New returns a CLI-friendly zap logger which prints to stderr by default.
@@ -26,13 +28,29 @@ func New(level zap.AtomicLevel, opts ...func(*Options)) *zap.Logger {
 
 	ec := zap.NewDevelopmentEncoderConfig()
 	ec.EncodeLevel = SymbolLevelEncoder
+
+	// if fileWriteSyncer is present then write logs to file as well as showing to console.
+	if o.FileWriteSyncer != nil {
+		fec := zap.NewProductionEncoderConfig()
+		fec.EncodeTime = zapcore.TimeEncoder(func(t time.Time, pae zapcore.PrimitiveArrayEncoder) {
+			pae.AppendString(t.UTC().Format("2023-01-02T15:04:05Z0700"))
+		})
+		fileEncoder := zapcore.NewJSONEncoder(fec)
+
+		core := zapcore.NewTee(zapcore.NewCore(fileEncoder, zapcore.AddSync(*o.FileWriteSyncer), level), zapcore.NewCore(NewConsoleEncoder(&ec, o.NoColor), zapcore.AddSync(o.Writer), level))
+
+		return zap.New(core)
+	}
+
 	// no-op time encoder, by default.
 	ec.EncodeTime = func(t time.Time, pae zapcore.PrimitiveArrayEncoder) {}
+
 	log := zap.New(zapcore.NewCore(
 		NewConsoleEncoder(&ec, o.NoColor),
 		zapcore.AddSync(o.Writer),
 		level,
 	))
+
 	return log
 }
 
@@ -47,5 +65,45 @@ func WithWriter(w io.Writer) func(*Options) {
 func WithNoColor(noColor *bool) func(*Options) {
 	return func(o *Options) {
 		o.NoColor = noColor
+	}
+}
+
+type FileLoggerConfig struct {
+	Filename       string
+	MaxSize        *int
+	MaxBackups     *int
+	MaxAge         *int
+	ShouldCompress *bool
+}
+
+func WithFileLogger(cfg FileLoggerConfig) func(*Options) {
+	loggerCfg := lumberjack.Logger{
+		Filename:   cfg.Filename,
+		MaxSize:    1,
+		MaxBackups: 30,
+		MaxAge:     90,
+		Compress:   false,
+	}
+
+	if cfg.MaxSize != nil {
+		loggerCfg.MaxSize = *cfg.MaxSize
+	}
+
+	if cfg.MaxAge != nil {
+		loggerCfg.MaxAge = *cfg.MaxAge
+	}
+
+	if cfg.MaxBackups != nil {
+		loggerCfg.MaxBackups = *cfg.MaxBackups
+	}
+
+	if cfg.ShouldCompress != nil {
+		loggerCfg.Compress = *cfg.ShouldCompress
+	}
+
+	ws := zapcore.AddSync(&loggerCfg)
+
+	return func(o *Options) {
+		o.FileWriteSyncer = &ws
 	}
 }
